53bed69f5e4b79897091896280b6adb9d3d51535
Ally MacLeod <155204136+amacleod1417@users.noreply.github.com>
2024-03-19 21:34:32 -0400

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <button v-on:click="toggle='character-viewer'">View all characters</button>
+        <button v-on:click="toggle='character-creator'">Create a character</button>
+        <CharacterViewer v-show="toggle==='character-viewer'" />
+        <CharacterCreator v-show="toggle==='character-creator'" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import CharacterViewer from './components/CharacterViewer.vue'
+    import CharacterCreator from './components/CharacterCreator.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            CharacterViewer,
+            CharacterCreator
+        },
+        data: function () {
+            return {
+                toggle: "character-viewer"
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/CharacterCreator.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="character-creator">
+        <h1>Character Creator</h1>
+        <label for="character-name">Character Name: </label>
+        <input type="text" id="character-name" v-model="name" placeholder="Enter a name" /> <br /><br />
+        <label for="professions-list">Character Profession: </label>
+        <select id="professions-list" v-model="profession">
+            <option value="Wizard">Wizard</option>
+            <option value="Spy">Spy</option>
+            <option value="Warrior">Warrior</option>
+            <option value="Blacksmith">Blacksmith</option>
+        </select>
+        <p>{{name}}</p>
+        <p>{{profession}}</p>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'CharacterCreator',
+        data: function () {
+            return {
+                name: null,
+                profession: null
+            }
+        },
+        methods: {
+            postCharacter: function () {
+                axios
+                    .post('http://localhost:3000/characters', {
+                        name: this.name,
+                        profession: this.profession
+                    });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
